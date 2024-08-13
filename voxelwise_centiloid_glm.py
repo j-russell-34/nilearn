4799e26c4ccea77918c0edc7102089a0577a355f
Jason Russell <jason.russell34@gmail.com>
2024-08-13 16:28:42 -0500

    Updated centiloid script to include non-paramatric inference by cluster size
    Using the non-paramatric inference in nilearn significant clusters
    are detected. Significance levels are coded as variables at the top
    of the script that can be easily editted, along with number of
    permutations for the non-parametric inference (500 for on computer,
    10000 when on cluster). Additionally, produced pdf output display
    the results of different statistical tests.

--- voxelwise_centiloid_glm.py
+++ voxelwise_centiloid_glm.py
@@ -9,28 +9,42 @@ Script to perform Voxel-wise linear regression between FEOBV and centiloid
 
 from nilearn import image
 from nilearn.glm.second_level import SecondLevelModel
+from nilearn.glm.second_level import non_parametric_inference
 from nilearn.glm import threshold_stats_img
 import pandas as pd
 import os
 import numpy as np
 from nilearn import plotting
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages as pdf
+from nilearn.image import new_img_like
 
 #Set path where data is stored
 data_path = '/home/jason/Study_data/Down Syndrome/TRCDS/Raw_images/DSCHOL-A003-2024-07-15a/DATA'
 
 os.chdir(data_path)
+output_path = '/home/jason/Study_data/Down Syndrome/Outputs'
 
+#set signficant thresholds for different tests
+#significance (p-val) for initial test at cluster threshold 50
+threshold_1 = 0.005
+threshold_non_para = 0.005
+#significance of clusters following non-parametric inference
+cluster_thres = -np.log10(0.05)
+
+#set number of permutations for non-parametric inference (10000 when finalized
+# but adds compute time, 500 for running on computer)
+permutations = 10000
 
 # Load PET images
 FEOBV_img_paths = [
-    'DST3050001/swFEOBV.nii', 'DST3050002/swFEOBV.nii',
-    'DST3050003/swFEOBV.nii', 'DST3050012/swFEOBV.nii',
-    'DST3050033/swFEOBV.nii', 'DST3050041/swFEOBV.nii',
-    'DST3050042/swFEOBV.nii', 'DST3050045/swFEOBV.nii',
-    'DST3050052/swFEOBV.nii', 'DST3050059/swFEOBV.nii',
-    'DST3050060/swFEOBV.nii', 'DST3050061/swFEOBV.nii',
-    'DST3050071/swFEOBV.nii'
+	'DST3050001/swFEOBV.nii', 'DST3050002/swFEOBV.nii',
+	'DST3050003/swFEOBV.nii', 'DST3050012/swFEOBV.nii',
+	'DST3050033/swFEOBV.nii', 'DST3050041/swFEOBV.nii',
+	'DST3050042/swFEOBV.nii', 'DST3050045/swFEOBV.nii',
+	'DST3050052/swFEOBV.nii', 'DST3050059/swFEOBV.nii',
+	'DST3050060/swFEOBV.nii', 'DST3050061/swFEOBV.nii',
+	'DST3050071/swFEOBV.nii'
 ]
 
 # Load NIfTI images into a 4D image
@@ -45,8 +59,8 @@ dimx, dimy, dimz, subjects = FEOBV_imgs.shape
 
 
 design_matrix = pd.DataFrame({
-    "centiloid": centiloid,
-    "intercept": np.ones(subjects)
+	"centiloid": centiloid,
+	"intercept": np.ones(subjects)
 })
 
 
@@ -55,35 +69,99 @@ gmmask_path = '/home/jason/Study_data/Down Syndrome/TRCDS/Raw_images/DSCHOL-A003
 
 #second level model
 second_level_model = SecondLevelModel(
-    mask_img=gmmask_path, n_jobs=1
+	mask_img=gmmask_path, n_jobs=1
 )
 second_level_model = second_level_model.fit(
-    FEOBV_img_paths, design_matrix=design_matrix)
+	FEOBV_img_paths, design_matrix=design_matrix)
 
 
 #calculate zmaps
 z_map = second_level_model.compute_contrast(
-    second_level_contrast=[1, 0],
-    output_type="z_score",
+	second_level_contrast=[1, 0],
+	output_type="z_score",
 )
 
 
 # Perform statsmap, correct for multiple comparisons
-thresholded_map, threshold = threshold_stats_img(z_map, alpha=0.005)
-
-fig = plt.figure(figsize=(5, 3))
-display = plotting.plot_stat_map(
-    z_map,
-    threshold=threshold,
-    colorbar=True,
-    display_mode="z",
-    figure=fig,
-)
-fig.suptitle("centiloid effect on FEOBV uptake (p = 0.005)")
-plotting.show()
+thresholded_map, threshold = threshold_stats_img(z_map,
+												 alpha=threshold_1,
+												 cluster_threshold=50)
 
 # Save the statistical map
 # Save the thresholded z-map to a NIfTI file
-thresholded_map.to_filename('thresholded_centiloid_effect_z_map.nii')
+thresholded_map.to_filename(f'{output_path}/Centiloid_glm_zmap.nii')
+
+#perform non parametric inference
+corrected_map = non_parametric_inference(
+	FEOBV_img_paths,
+	design_matrix=design_matrix,
+	second_level_contrast=[1,0],
+	mask=gmmask_path,
+	n_perm=permutations,
+	two_sided_test=True,
+	n_jobs=1,
+	threshold=threshold_non_para
+	)
+
+# extract cluster significance <0.05
+img_data_non_para = corrected_map['logp_max_size'].get_fdata()
+img_data_non_para[img_data_non_para < cluster_thres] = 0
+img_data_non_para_mask = img_data_non_para != 0
+thresholded_map_np = np.where(img_data_non_para_mask, img_data_non_para, np.nan)
+
+thresholded_map_np_ni = new_img_like('DST3050001/swFEOBV.nii', thresholded_map_np)
+
+# Save non-parametric inference corrected map
+thresholded_map_np_ni.to_filename(f'{output_path}/Centiloid_glm_non_parametric_inference_corrected_logP_map.nii')
+
+# Generate pdf report
+pdf_filename = "Centiloid_FEOBV_GLM.pdf"
+
+fig, axs = plt.subplots(3,1, figsize=(10,14))
+
+plotting.plot_stat_map(
+	thresholded_map,
+	threshold=threshold,
+	colorbar=True,
+	cut_coords=6,
+	display_mode="x",
+	figure=fig,
+	title = "GLM output p < 0.005, cluster size 50 (z-scores)",
+	axes=axs[0]
+)
+
+plotting.plot_stat_map(
+	corrected_map['logp_max_size'],
+	colorbar=True,
+	vmax=-np.log10(1 / permutations),
+	threshold = cluster_thres,
+	cut_coords=6,
+	display_mode="x",
+	figure=fig,
+	title = "GLM output p < 0.005, non-parametic inference, cluster size (cluster logP)",
+	axes=axs[1]
+)
+
+plotting.plot_stat_map(
+	corrected_map['logp_max_mass'],
+	colorbar=True,
+	vmax=-np.log10(1 / permutations),
+	threshold = cluster_thres,
+	cut_coords=6,
+	display_mode="x",
+	figure=fig,
+	title = "GLM output p < 0.005, non-parametic inference, cluster mass (cluster logP)",
+	axes=axs[2]
+)
+
+
+
+fig.suptitle("Association Between Centiloid Value and FEOBV uptake")
+
+
+pdf.savefig(f'{output_path}/{pdf_filename}', dpi=300)
+plt.close()
+
+
 
 
